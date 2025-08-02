358acdee408dc7832fcdbd6e37f633da8a2043e5
Enzo Luís Raspante <enzoraspante042@gamil.com>
2025-08-02 11:35:42 -0300

    Projeto realizado nas aulas ede python-II no Senac

---
+++ popula_pratos.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO churras_prato (nome_prato, ingredientes, modo_preparo, tempo_preparo, rendimento, categoria, data_prato) VALUES
+('Carne de Sol', 'Carne de sol dessalgada, Cebola', 'Dessalgar, refogar com cebola e grelhar.', 50, 3, 'Carne Vermelha', '2025-06-27 14:00:00');
